53986e4a839dbeb94005dfd1990eba472cf9d2a3
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-17 21:26:29 -0600

    add and design hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
-    
+      <section className="pink_container">
+        <p className="tag">PITCH, VOTE, AND GROW</p>
+        <h1 className="heading">
+          PITCH YOUR STARTUP, <br /> 
+          CONNECT WITH ENTREPRENEURS
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
     </>
   );
 }
